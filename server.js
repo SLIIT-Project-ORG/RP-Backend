51762c83e8bb245bd8ddfbac499cd8c731f1a1aa
Tharuka Gayashan <tharuka.gayashan.5@gmail.com>
2023-05-06 12:07:54 +0530

    project init

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+require("./config/DBConnection");
+
+const app = express();
+app.use(cors())
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT;
+
+app.listen(PORT,()=>{
+    console.log(`Application listen to ${PORT} port`);
+})
